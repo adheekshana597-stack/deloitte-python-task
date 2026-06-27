ccdd82c0b6f1fee02be665296466c1a0f5d59bd7
adheekshana597-stack <adheekshana597@gmail.com>
2026-06-27 07:49:24 +0530

    add data cleaning script

---
+++ data_cleaning .py
@@ -0,0 +1,14 @@
+{
+  "device_id": "dh28dslkja",
+  "device_type": "LaserCutter",
+  "timestamp_utc": "2021-06-23T10:57:17.783Z",
+
+  "location_country": "Japan",
+  "location_city": "Tokyo",
+  "location_area": "Keiyō Industrial Zone",
+  "location_factory": "Daikibo Factory Meiyo",
+  "location_section": "Section 1",
+
+  "status": "HEALTHY",
+  "temperature_celsius": 22
+}
